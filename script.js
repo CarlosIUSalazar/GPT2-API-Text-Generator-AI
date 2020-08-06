365b9c47283a9827245354e968b002c0a654d546
Carlos Salazar <carlosiusalazar@gmail.com>
2020-08-06 13:59:28 +0900

    Added styles.css

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-let text = document.getElementById("myText")
-let output = document.getElementById("myText2")
+let text = document.getElementById("myText");
+let output = document.getElementById("myText2");
 
-text.value = ""
-output.value = ""
+text.value = "";
+output.value = "";
 
-function submitText(){
-    text = document.getElementById("myText").value
-    console.log(text)
-    output.value = "Please wait...";
-    deepai.setApiKey('85b4427e-815e-4efd-99e2-de892b8f2d22');
-    (async function() {
+function submitText() {
+  text = document.getElementById("myText").value;
+  console.log(text);
+  output.value = "Please wait...";
+  deepai.setApiKey("85b4427e-815e-4efd-99e2-de892b8f2d22");
+  (async function () {
     var resp = await deepai.callStandardApi("text-generator", {
-            text: text,
-    });    
+      text: text,
+    });
     await console.log(resp);
     output.value = resp.output;
-})()
-
+  })();
 }
